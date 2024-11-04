97c1022d4f3ea14ccb2e79f643b82f6c6d9e06a1
caninobranco <caninobranco11@gmail.com>
2024-11-04 03:39:05 -0300

    deployado

---
+++ contracts/Cert.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Cert is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address public owner;
+    mapping(uint256 => bool) private _transferable;
+    mapping(string => uint256) private _tokenURIs;
+
+    constructor() ERC721("Cert", "CER") {
+        owner = msg.sender;
+    }
+
+
+
+    function awardCertificate(address student, string memory tokenURI) public returns (uint256) {
+        require(student != address(0), "Invalid student address");
+
+        uint256 newCertificateId = _tokenIds.current();
+        _mint(student, newCertificateId); // Isso chamará automaticamente _beforeTokenTransfer
+        _setTokenURI(newCertificateId, tokenURI);
+        _transferable[newCertificateId] = false; // Marca o certificado como não transferível
+        _tokenIds.increment();
+        _tokenURIs[tokenURI] = newCertificateId;
+        return newCertificateId;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal  {
+        require(!_transferable[tokenId], "Certificate is not transferable");
+    }
+
+    function getTokenId(string memory tokenURI) public view returns (uint256) {
+        return _tokenURIs[tokenURI]; // Retorna o tokenId associado ao tokenURI
+    }
+
+    function getTokenURI(uint256 tokenId) public view returns (string memory) {
+        string memory uri = super.tokenURI(tokenId);
+        require(bytes(uri).length > 0, "Token does not have an associated URI");
+        return uri;
+    }
+}
---
+++ ignition/modules/contract.ts
@@ -0,0 +1,14 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+const CertModule = buildModule("CertModule", (m) => {
+
+  const contract = m.contract("Cert");
+
+  return { contract };
+});
+
+export default CertModule;
